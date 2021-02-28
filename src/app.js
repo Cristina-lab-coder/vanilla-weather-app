2a27feabf8836a34e808dfe7fa63a1f2873e64d1
Cristina-lab-coder <76906522+Cristina-lab-coder@users.noreply.github.com>
2021-02-28 13:22:43 +0100

    Added the search engine

--- src/app.js
+++ src/app.js
@@ -39,10 +39,22 @@ function displayTemperature(response) {
     iconElement.setAttribute("alt", response.data.weather[0].description );
 }
 
-
+function search(city) {
 let apiKey = "27eeecfd299bc2b1de7653a8ec484054";
-let city = "New York";
 let apiUrl =`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-
-
 axios.get(apiUrl).then(displayTemperature);
+}
+
+function handleSubmit(event) {
+event.preventDefault();
+let cityInputElement = document.querySelector("#city-input");
+search(cityInputElement.value);
+}
+
+search("New York");
+
+
+
+
+let form =document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
